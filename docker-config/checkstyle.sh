cf01c61d8a584bd3e7356c6a1c26f43bfa48dccf
Erik Seifert <erik.seifert@b-connect.de>
2016-08-11 13:22:11 +0200

    Correct path

--- docker-config/checkstyle.sh
+++ docker-config/checkstyle.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 mkdir /results/phpcs
-phpcs --standard=Drupal --report=xml --report-file=/result/phpcs/result.xml --extensions=php,module,inc,install,test,profile,theme,js,css,info,txt,md ${DRONE_DIR}
+phpcs --standard=Drupal --report=xml --report-file=/results/phpcs/result.xml --extensions=php,module,inc,install,test,profile,theme,js,css,info,txt,md ${DRONE_DIR}
 
 mkdir /results/phpmetrics
 phpmetrics --report-html=/results/phpmetrics/result.html ${DRONE_DIR}
