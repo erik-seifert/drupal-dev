1346515343feae320fd14c8b829c901f52576337
Erik Seifert <erik.seifert@b-connect.de>
2016-08-15 15:53:18 +0200

    Correct file path

--- docker-config/checkstyle.sh
+++ docker-config/checkstyle.sh
@@ -4,7 +4,7 @@ echo "RUN METRICS ON ${DRONE_DIR}/${SRC_DIR}"
 
 phpcs --version
 mkdir /results/phpcs
-phpcs --standard=Drupal --report=xml --report-file=/results/phpcs/result.xml --extensions=php,module,inc,install,test,profile,theme,js,css,info,txt,md ${DRONE_DIR}
+phpcs --standard=Drupal --report=xml --report-file=/results/phpcs/result.xml --extensions=php,module,inc,install,test,profile,theme,js,css,info,txt,md ${DRONE_DIR}/${SRC_DIR}
 
 phpmetrics --version
 mkdir /results/phpmetrics
